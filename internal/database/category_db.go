5735aa1c58d784d2a94d3fd2bca0dbc37850ddf4
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-02-02 22:08:25 -0300

    feat: add method to create category

--- internal/database/category_db.go
+++ internal/database/category_db.go
@@ -34,3 +34,13 @@ func (cd *CategoryDB) GetCategories() ([]*entity.Category, error) {
 
 	return categories, nil
 }
+
+func (cd *CategoryDB) CreateCategory(category *entity.Category) (string, error) {
+	_, err := cd.db.Exec("INSERT INTO categories (id, name) VALUES (?, ?)", category.ID, category.Name)
+
+	if err != nil {
+		return "", err
+	}
+
+	return category.ID, nil
+}
