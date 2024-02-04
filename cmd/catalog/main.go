17f65e025840ce8a894b15488d2d220918100fe9
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-02-03 21:27:04 -0300

    feat: add routes

---
+++ cmd/catalog/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/MariaGabrielaReis/ecobricks-product-catalog/internal/database"
+	"github.com/MariaGabrielaReis/ecobricks-product-catalog/internal/service"
+	"github.com/MariaGabrielaReis/ecobricks-product-catalog/internal/webserver"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/ecobricks")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategoryDB(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+	webCategoryHandler := webserver.NewWebCategoryHandler(categoryService)
+
+	productDB := database.NewProductDB(db)
+	productService := service.NewProductService(*productDB)
+	webProductHandler := webserver.NewWebProductHandler(productService)
+
+	c := chi.NewRouter()
+
+	c.Use(middleware.Logger)
+	c.Use(middleware.Recoverer)
+
+	c.Get("/category", webCategoryHandler.GetCategories)
+	c.Get("/category/{id}", webCategoryHandler.GetCategory)
+	c.Post("/category", webCategoryHandler.CreateCategory)
+
+	c.Get("/product", webProductHandler.GetProduct)
+	c.Get("/product/{id}", webProductHandler.GetProduct)
+	c.Get("/product/category/{categoryID}", webProductHandler.GetProductsByCategory)
+	c.Post("/product", webProductHandler.CreateProduct)
+
+	fmt.Println("Server is running on port 8080 🔥")
+	http.ListenAndServe(":8080", c)
+}
